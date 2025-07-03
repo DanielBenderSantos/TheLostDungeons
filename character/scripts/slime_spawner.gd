672f4730653dc07d26f7418f8ccfe9e87409ba9a
Yokai <yokaiaatrox@gmail.com>
2025-07-02 21:42:55 -0300

    jogo base+caves

--- character/scripts/slime_spawner.gd
+++ character/scripts/slime_spawner.gd
@@ -1,21 +1,32 @@
 extends Node2D
 
 @export var slime_scene: PackedScene
-@export var spawn_timer: float = 5.0
+@export var spawn_timer: float = 1.0
+
+var current_slime = null
+var timer = null
 
 func _ready():
 	spawn_slime()
-	start_timer()
-
-func start_timer():
-	var timer = Timer.new()
-	timer.wait_time = spawn_timer
-	timer.one_shot = false
-	timer.connect("timeout", Callable(self, "spawn_slime"))
-	add_child(timer)
-	timer.start()
 
 func spawn_slime():
-	var slime = slime_scene.instantiate()
-	slime.global_position = self.global_position
-	get_parent().add_child(slime)
+	# Cria o slime
+	current_slime = slime_scene.instantiate()
+	current_slime.global_position = self.global_position
+	get_parent().add_child.call_deferred(current_slime)
+
+	# Conecta o sinal de morte do slime
+	current_slime.connect("slime_died", Callable(self, "_on_slime_died"))
+
+func _on_slime_died():
+	current_slime = null
+
+	# Cria e inicia o timer para o próximo slime
+	if timer == null:
+		timer = Timer.new()
+		add_child(timer)
+
+	timer.wait_time = spawn_timer
+	timer.one_shot = true
+	timer.connect("timeout", Callable(self, "spawn_slime"))
+	timer.start()
