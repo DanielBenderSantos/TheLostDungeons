9a154936bff28f07669e9e22c27f40a795f16b0a
DanielBender <danielbendersantos@gmail.com>
2025-07-06 14:16:01 -0300

    add audio

--- levels/scripts/dialog_screen.gd
+++ levels/scripts/dialog_screen.gd
@@ -7,8 +7,10 @@ signal dialog_finished
 @export var _faceset: TextureRect
 @export var _name: Label
 @export var _dialog: RichTextLabel
+@export var narration_player: AudioStreamPlayer  # Referência para o player de áudio
 
 var data: Dictionary = {}
+var audio: Dictionary = {}  # Dicionário dos áudios recebidos
 var _id: int = 0
 var _step: float = 0.05
 var _is_skipping: bool = false
@@ -27,6 +29,7 @@ func _process(_delta: float) -> void:
 
 	if Input.is_action_just_pressed("ui_accept"):
 		if _dialog.visible_ratio < 1:
+			narration_player.stop()  # Para o áudio quando pular o texto
 			_dialog.visible_characters = _dialog.text.length()
 			return
 
@@ -47,7 +50,13 @@ func _initialize_dialog() -> void:
 	var dialog_entry = data[_id]
 	_name.text = dialog_entry.get("title", "Sem Título")
 	_dialog.text = dialog_entry.get("dialog", "...")
+
 	_faceset.texture = load(dialog_entry.get("faceset", "res://default.png"))
+
+	# Tocar o áudio correspondente
+	if audio.has(_id):
+		narration_player.stream = audio[_id]
+		narration_player.play()
 
 	_dialog.visible_characters = 0
 
